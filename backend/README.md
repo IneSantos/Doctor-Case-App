507eceab65d56e15a9a748c4fc0d9fc7b65ba1f0
Inês Carneiro <ines.carneiro@mindera.com>
2021-04-10 17:47:08 +0100

    Added connection to Data Base

---
+++ backend/README.md
@@ -0,0 +1,1 @@
+# Doctor-Case-App Backend
--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 
-app.listen(3000);
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useUnifiedTopology: true } ,
+    () => console.log('connected to db')
+)
+
+//Port to the application
+app.listen(process.env.APP_PORT);
